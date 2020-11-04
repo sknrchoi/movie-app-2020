e73da38ffd99ac1689231c097d846d342db222bf
sknrchoi <seulkinara@gmail.com>
2020-11-04 12:50:17 +0900

    #6.6 Detail page - content setting

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
     componentDidMount() {
@@ -11,22 +12,24 @@ class Detail extends React.Component {
         const { location } = this.props;
         if(location.state) {
             return (
-                <div>
-                    <img src={location.state.poster} alt={location.state.title} title={location.state.title}></img>
-                    <div>
-                        <h2>{location.state.title} ( {location.state.year} )</h2>
-                        <ul>
-                            {typeof location.state.genres !== 'undefined' ? (
-                                location.state.genres.map((genre, index) => (
-                                    <li key={index} >{genre}</li>
-                                ))) : (
-                                    <li></li>
-                                )
-                            }
-                        </ul> 
-                        <p>{location.state.summary}</p>                       
+                <section className="detail__container">
+                    <div className="detail__movie">
+                        <img src={location.state.poster} alt={location.state.title} title={location.state.title}></img>
+                        <div>
+                            <h2>{location.state.title} ( {location.state.year} )</h2>
+                            <ul>
+                                {typeof location.state.genres !== 'undefined' ? (
+                                    location.state.genres.map((genre, index) => (
+                                        <li key={index} >{genre}</li>
+                                    ))) : (
+                                        <li></li>
+                                    )
+                                }
+                            </ul> 
+                            <p>{location.state.summary}</p>                       
+                        </div>
                     </div>
-                </div>
+                </section>
             );
         } else {
             return null;
