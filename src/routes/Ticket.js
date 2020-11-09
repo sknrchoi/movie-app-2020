a052cb48f21af730c6b167093de6932154e469b8
sknrchoi <seulkinara@gmail.com>
2020-11-09 15:33:07 +0900

    #6.9 Modify ticket page

--- src/routes/Ticket.js
+++ src/routes/Ticket.js
@@ -1,8 +1,68 @@
-import React from "react"
+import React from "react";
+import axios from "axios";
+import "./Ticket.css";
 
 class Ticket extends React.Component {
+    state = {
+        isLoading: true,
+        movies: []
+    }
+    getMovies = async () => {
+        const {
+            data: {
+                data: {
+                    movies
+                }
+            }
+        } = await axios.get(
+            "https://yts-proxy.now.sh/list_movies.json"
+        );
+        this.setState({movies, isLoading: false});
+    }
+    componentDidMount() {
+        this.getMovies();
+    }
+
+    selectMovie = () => {
+        console.log("movie!");
+    };
     render() {
-        return <span>ticket page for reservation</span>;
+        const {isLoading, movies} = this.state;
+        return (
+            <section className="ticket__container">
+            {isLoading ? (
+                <div className="loader">
+                    <span className="loader__text">Loading...</span>
+                </div>
+            ) : (
+                <article className="step">
+                    <div className="step__movie">
+                        <div className="head">
+                            MOVIE
+                        </div>
+                        <div className="body">
+                            <ul>
+                                {movies.map(movie => (
+                                <li key={movie.id} title={movie.title} onClick={this.selectMovie}>
+                                    {movie.title}
+                                </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="step__date">
+                        <div className="head">
+                            DATE
+                        </div>
+                        <div className="body">
+
+                        </div>
+
+                    </div>
+                </article>
+            )}
+            </section>
+        );
     }
 }
 
